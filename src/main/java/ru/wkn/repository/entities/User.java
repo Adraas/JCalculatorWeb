5dde2633f74db0dabaf4326741d00976eb90e103
Adraas <pickalov.artyom@yandex.ru>
2019-03-16 09:17:53 +0400

    New entities for mapping

---
+++ src/main/java/ru/wkn/repository/entities/User.java
@@ -0,0 +1,10 @@
+package ru.wkn.repository.entities;
+
+public class User {
+
+    private long id;
+    private String fullName;
+    private String login;
+    private String password;
+    private String cookie;
+}
