52e7dad393c52ee85ea45c4d1fbc56ac3804705b
Adraas <pickalov.artyom@yandex.ru>
2019-03-18 16:22:28 +0400

    H2OperationDao is ready

--- repository/src/main/java/ru/wkn/repository/h2/H2OperationDao.java
+++ repository/src/main/java/ru/wkn/repository/h2/H2OperationDao.java
@@ -1,10 +1,14 @@
 package ru.wkn.repository.h2;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import ru.wkn.entities.Operation;
 import ru.wkn.repository.IDao;
 
 import java.util.List;
+import java.util.Objects;
 
 public class H2OperationDao implements IDao<Operation, Integer> {
 
@@ -15,24 +19,51 @@ public class H2OperationDao implements IDao<Operation, Integer> {
     }
 
     @Override
-    public void create(Operation newInstance) {
+    public void create(Operation newOperation) {
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.save(newOperation);
+            transaction.commit();
+        } catch (HibernateException e) {
+            Objects.requireNonNull(transaction).rollback();
+        }
     }
 
     @Override
     public Operation read(Integer index) {
-        return null;
+        return (Operation) session.get(Operation.class, index);
     }
 
     @Override
-    public void update(Operation transientInstance) {
+    public void update(Operation transientOperation) {
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.update(transientOperation);
+            transaction.commit();
+        } catch (HibernateException e) {
+            Objects.requireNonNull(transaction).rollback();
+        }
     }
 
     @Override
-    public void delete(Operation transientInstance) {
+    public void delete(Operation transientOperation) {
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.delete(transientOperation);
+            transaction.commit();
+        } catch (HibernateException e) {
+            Objects.requireNonNull(transaction).rollback();
+        }
     }
 
     @Override
     public List<Operation> getAll() {
-        return null;
+        List<Operation> vList;
+        Query query = session.createQuery("SELECT * FROM operation");
+        vList = (List<Operation>) query.list();
+        return vList;
     }
 }
