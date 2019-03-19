1eb956be0ead1bedc49330d464ccd37378b9edf3
Adraas <pickalov.artyom@yandex.ru>
2019-03-19 07:15:28 +0400

    Service for DAO was created and init

---
+++ repository/src/main/java/ru/wkn/repository/IService.java
@@ -0,0 +1,4 @@
+package ru.wkn.repository;
+
+public interface IService<V, I> extends IDao<V, I> {
+}
---
+++ repository/src/main/java/ru/wkn/repository/service/Service.java
@@ -0,0 +1,40 @@
+package ru.wkn.repository.service;
+
+import ru.wkn.repository.IDao;
+import ru.wkn.repository.IService;
+
+import java.util.List;
+
+public class Service<V, I> implements IService<V, I> {
+
+    private IDao<V, I> iDao;
+
+    public Service(IDao<V, I> iDao) {
+        this.iDao = iDao;
+    }
+
+    @Override
+    public void create(V newInstance) {
+        iDao.create(newInstance);
+    }
+
+    @Override
+    public V read(I index) {
+        return iDao.read(index);
+    }
+
+    @Override
+    public void update(V transientInstance) {
+        iDao.update(transientInstance);
+    }
+
+    @Override
+    public void delete(V transientInstance) {
+        iDao.delete(transientInstance);
+    }
+
+    @Override
+    public List<V> getAll(String table) {
+        return iDao.getAll(table);
+    }
+}
