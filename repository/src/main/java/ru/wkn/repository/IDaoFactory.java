eaec6ce5feebd1e791319f368c19051cdc74b543
Adraas <pickalov.artyom@yandex.ru>
2019-03-22 11:38:33 +0400

    Parameter was renamed

--- repository/src/main/java/ru/wkn/repository/IDaoFactory.java
+++ repository/src/main/java/ru/wkn/repository/IDaoFactory.java
@@ -4,5 +4,5 @@ import org.hibernate.Session;
 
 public interface IDaoFactory<V, I> {
 
-    IDao<V, I> createDao(Class<IDao<V, I>> database, Class<V> entityClass, Session session);
+    IDao<V, I> createDao(Class<IDao<V, I>> daoClass, Class<V> entityClass, Session session);
 }
