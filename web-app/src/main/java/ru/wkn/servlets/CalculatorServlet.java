f58c9eed1cc7e7ddd0103ec08674cb918eb1a91c
Adraas <pickalov.artyom@yandex.ru>
2019-04-05 22:16:44 +0400

    New servlets was created

---
+++ web-app/src/main/java/ru/wkn/servlets/CalculatorServlet.java
@@ -0,0 +1,6 @@
+package ru.wkn.servlets;
+
+import javax.servlet.http.HttpServlet;
+
+public class CalculatorServlet extends HttpServlet {
+}
---
+++ web-app/src/main/java/ru/wkn/servlets/SignInServlet.java
@@ -0,0 +1,6 @@
+package ru.wkn.servlets;
+
+import javax.servlet.http.HttpServlet;
+
+public class SignInServlet extends HttpServlet {
+}
