3baf24311d2f1fe2c08b1b4d0b365551e4407da4
Adraas <pickalov.artyom@yandex.ru>
2019-04-09 09:39:30 +0400

    Servlet for signIn JSP is ready

--- web-app/src/main/java/ru/wkn/servlets/SignInServlet.java
+++ web-app/src/main/java/ru/wkn/servlets/SignInServlet.java
@@ -1,6 +1,34 @@
 package ru.wkn.servlets;
 
+import ru.wkn.RepositoryFacade;
+import ru.wkn.entities.User;
+import ru.wkn.repository.dao.h2.H2Dao;
+import ru.wkn.repository.service.UserService;
+
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class SignInServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        Class h2DaoClass = H2Dao.class;
+        Class userClass = User.class;
+        RepositoryFacade<User, Integer> repositoryFacade = new RepositoryFacade<>(h2DaoClass, userClass);
+
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
+        User user = ((UserService) repositoryFacade.getService()).logIn(login, password);
+
+        resp.setContentType("text/html");
+        if (user != null) {
+            resp.addCookie(new Cookie("user", user.getCookie()));
+            resp.getWriter().println("Log in successful!");
+        } else {
+            resp.getWriter().println("User not found!");
+        }
+    }
 }
