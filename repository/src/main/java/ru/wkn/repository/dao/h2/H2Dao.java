c48e95f75f5ceb872d6f77a6113645055fc0a4d8
Adraas <pickalov.artyom@yandex.ru>
2019-04-11 11:00:29 +0400

    Basic settings for logging for H2Dao

--- repository/src/main/java/ru/wkn/repository/dao/h2/H2Dao.java
+++ repository/src/main/java/ru/wkn/repository/dao/h2/H2Dao.java
@@ -10,11 +10,13 @@ import javax.persistence.Table;
 import java.io.Serializable;
 import java.util.List;
 import java.util.Objects;
+import java.util.logging.Logger;
 
 public class H2Dao<V, I extends Serializable> implements IDao<V, I> {
 
     private Class<V> entityClass;
     private Session session;
+    private static final Logger logger = Logger.getLogger(H2Dao.class.getName());
 
     public H2Dao(Class<V> entityClass, Session session) {
         this.entityClass = entityClass;
@@ -29,6 +31,7 @@ public class H2Dao<V, I extends Serializable> implements IDao<V, I> {
             session.save(newInstance);
             transaction.commit();
         } catch (HibernateException e) {
+            logger.warning("Element ".concat(newInstance.toString()).concat(" not created"));
             Objects.requireNonNull(transaction).rollback();
             return false;
         }
@@ -48,6 +51,7 @@ public class H2Dao<V, I extends Serializable> implements IDao<V, I> {
             session.update(transientInstance);
             transaction.commit();
         } catch (HibernateException e) {
+            logger.warning("Element ".concat(transientInstance.toString()).concat(" not updated"));
             Objects.requireNonNull(transaction).rollback();
         }
     }
@@ -60,6 +64,7 @@ public class H2Dao<V, I extends Serializable> implements IDao<V, I> {
             session.delete(transientInstance);
             transaction.commit();
         } catch (HibernateException e) {
+            logger.warning("Element ".concat(transientInstance.toString()).concat(" not deleted"));
             Objects.requireNonNull(transaction).rollback();
         }
     }
