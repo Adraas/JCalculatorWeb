b5db379a7be8019b3904f090b899b9120bca860e
Adraas <pickalov.artyom@yandex.ru>
2019-04-11 22:13:10 +0400

    Calculator servlet logic is almost ready

--- web-app/src/main/java/ru/wkn/servlets/CalculatorServlet.java
+++ web-app/src/main/java/ru/wkn/servlets/CalculatorServlet.java
@@ -11,40 +11,60 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CalculatorServlet extends HttpServlet {
 
+    private static Map<String, Operation> cookieOperationMap = new HashMap<>();
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String expression = req.getParameter("expression");
+        String cookie = req.getParameter("cookie");
+        Operation operation = getOperationByCookie(cookie);
+
+        char symbol = req.getParameter("symbol").toCharArray()[0];
         int roundingAccuracy = Integer.parseInt(req.getParameter("rounding_accuracy"));
 
         CalculatorFacade calculatorFacade = new CalculatorFacade(roundingAccuracy);
         CalculatorExpressionCompiler calculatorExpressionCompiler = calculatorFacade.getCalculatorExpressionCompiler();
 
-        if (expression.equals("C")) {
+        if (symbol == 'C') {
             calculatorExpressionCompiler.allClear();
+            resp.setHeader("result", "0");
         } else {
-            String cookie = req.getParameter("cookie");
-            String result = "";
-            char[] expressionAsCharArray = expression.toCharArray();
-            for (char symbol : expressionAsCharArray) {
-                result = result.concat(calculatorExpressionCompiler.getCurrentAnswerAsString(symbol));
+            String result = calculatorExpressionCompiler.getCurrentAnswerAsString(symbol);
+            boolean isSaved = false;
+            if (operationReady(operation)) {
+                operation.setOperationResult(result);
+                isSaved = saveOperation(operation);
             }
-            req.setAttribute("result", result);
-            req.getRequestDispatcher("/calculator/calculator.jsp").forward(req, resp);
-            if (cookie == null || !saveOperation(expression.split("=")[0], result, cookie)) {
+            resp.setHeader("result", "0");
+            if (isSaved) {
                 resp.sendError(412, "Operation not created!");
             }
+            req.getRequestDispatcher("/calculator/calculator.jsp").forward(req, resp);
         }
     }
 
-    private boolean saveOperation(String content, String result, String cookie) {
+    private boolean saveOperation(Operation operation) {
+        cookieOperationMap.remove(operation.getCookie());
         Class<H2Dao> h2DaoClass = H2Dao.class;
         Class<Operation> operationClass = Operation.class;
         RepositoryFacade<Operation, Integer> repositoryFacade = new RepositoryFacade(h2DaoClass, operationClass);
-
-        Operation operation = new Operation(content, result, cookie);
         return repositoryFacade.getService().create(operation);
+    }
+
+    private Operation getOperationByCookie(String cookie) {
+        if (cookieOperationMap.containsKey(cookie)) {
+            return cookieOperationMap.get(cookie);
+        } else {
+            return new Operation("", null, cookie);
+        }
+    }
+
+    private boolean operationReady(Operation operation) {
+        //
+        return true;
     }
 }
