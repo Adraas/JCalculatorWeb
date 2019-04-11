f013c26d9fd9536da509ce4bc76ccad865a919f4
Adraas <pickalov.artyom@yandex.ru>
2019-04-11 10:09:43 +0400

    CalculatorFacade for model

---
+++ calculator/src/main/java/ru/wkn/calculator/CalculatorFacade.java
@@ -0,0 +1,30 @@
+package ru.wkn.calculator;
+
+import ru.wkn.calculator.calculating.Calculator;
+
+public class CalculatorFacade {
+
+    private Calculator calculator;
+    private CalculatorExpressionCompiler calculatorExpressionCompiler;
+
+    public CalculatorFacade(int roundingAccuracy) {
+        calculator = new Calculator(roundingAccuracy);
+        calculatorExpressionCompiler = new CalculatorExpressionCompiler(calculator);
+    }
+
+    public Calculator getCalculator() {
+        return calculator;
+    }
+
+    public void setCalculator(Calculator calculator) {
+        this.calculator = calculator;
+    }
+
+    public CalculatorExpressionCompiler getCalculatorExpressionCompiler() {
+        return calculatorExpressionCompiler;
+    }
+
+    public void setCalculatorExpressionCompiler(CalculatorExpressionCompiler calculatorExpressionCompiler) {
+        this.calculatorExpressionCompiler = calculatorExpressionCompiler;
+    }
+}
