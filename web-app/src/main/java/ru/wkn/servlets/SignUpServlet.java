b457cf0ac85bb315f2656721c580591121a82d2c
Adraas <pickalov.artyom@yandex.ru>
2019-06-22 20:27:30 +0400

    refactor: delete unnecessary errors throwing

--- web-app/src/main/java/ru/wkn/servlets/SignUpServlet.java
+++ web-app/src/main/java/ru/wkn/servlets/SignUpServlet.java
@@ -41,16 +41,16 @@ public class SignUpServlet extends HttpServlet {
                     if (isCreated) {
                         req.getRequestDispatcher("/sign_in.jsp").forward(req, resp);
                     } else {
-                        resp.sendError(400, "User not saved");
+                        resp.getWriter().println("User not saved");
                     }
                 } else {
-                    resp.sendError(400, "Input data contains forbidden symbols");
+                    resp.getWriter().println("Input data contains forbidden symbols");
                 }
             } else {
-                resp.sendError(400, "Input data contains forbidden symbols");
+                resp.getWriter().println("Input data contains forbidden symbols");
             }
         } else {
-            resp.sendError(400, "Missing required input data");
+            resp.getWriter().println("Missing required input data");
         }
     }
 
